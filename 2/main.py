e0469fbf12bacd60e06d5d3867dcfdb0dad16378
Charlie Brooking <cbrookin@thoughtworks.com>
2022-12-22 01:01:00 +1000

    add solution for day 2 part 2

--- 2/main.py
+++ 2/main.py
@@ -6,28 +6,45 @@ class Shape(Enum):
   PAPER = auto()
   SCISSORS = auto()
 
-def parse_round(line):
-  theirs = {'A': Shape.ROCK, 'B': Shape.PAPER, 'C': Shape.SCISSORS}
-  mine = {'X': Shape.ROCK, 'Y': Shape.PAPER, 'Z': Shape.SCISSORS}
+their_shape = {'A': Shape.ROCK, 'B': Shape.PAPER, 'C': Shape.SCISSORS}
+winner = {Shape.ROCK: Shape.PAPER, Shape.PAPER: Shape.SCISSORS, Shape.SCISSORS: Shape.ROCK}
+loser = {v: k for k, v in winner.items()}
+
+def parse_round_1(line):
+  my_shape = {'X': Shape.ROCK, 'Y': Shape.PAPER, 'Z': Shape.SCISSORS}
   round = line.split()
-  return (theirs[round[0]], mine[round[1]])
+  return (their_shape[round[0]], my_shape[round[1]])
+
+def parse_round_2(line):
+  round = line.split()
+  theirs = their_shape[round[0]]
+  mine = (
+    winner[theirs] if round[1] == 'Z' else
+    theirs if round[1] == 'Y' else
+    loser[theirs]
+  )
+  return (theirs, mine)
 
 def score_outcome(round):
-  winners = [(Shape.ROCK, Shape.PAPER), (Shape.PAPER, Shape.SCISSORS), (Shape.SCISSORS, Shape.ROCK)]
-  return 6 if round in winners else 3 if round[0] == round[1] else 0
+  return (
+    6 if round in winner.items() else
+    3 if round[0] == round[1] else
+    0
+  )
 
 def score_round(round):
   shape_score = {Shape.ROCK: 1, Shape.PAPER: 2, Shape.SCISSORS: 3}
   return score_outcome(round) + shape_score[round[1]]
 
-def total_score(lines):
+def total_score(lines, parse_round):
   rounds = [parse_round(line) for line in lines]
   scores = [score_round(round) for round in rounds]
   return sum(scores)
 
 def main():
   lines = stdin.read().splitlines()
-  print(total_score(lines))
+  print(total_score(lines, parse_round_1))
+  print(total_score(lines, parse_round_2))
 
 if __name__ == "__main__":
     main()
--- 2/test.py
+++ 2/test.py
@@ -1,11 +1,17 @@
 from unittest import TestCase
-from main import Shape, parse_round, score_outcome, score_round, total_score
+from main import Shape, parse_round_1, parse_round_2, score_outcome, score_round, total_score
 
 class Test(TestCase):
-  def test_parse_round(self):
-    self.assertEqual(parse_round('A Y'), (Shape.ROCK, Shape.PAPER))
-    self.assertEqual(parse_round('B X'), (Shape.PAPER, Shape.ROCK))
-    self.assertEqual(parse_round('C Z'), (Shape.SCISSORS, Shape.SCISSORS))
+  def test_parse_round_1(self):
+    self.assertEqual(parse_round_1('A Y'), (Shape.ROCK, Shape.PAPER))
+    self.assertEqual(parse_round_1('B X'), (Shape.PAPER, Shape.ROCK))
+    self.assertEqual(parse_round_1('C Z'), (Shape.SCISSORS, Shape.SCISSORS))
+
+  def test_parse_round_2(self):
+    self.assertEqual(parse_round_2('A Y'), (Shape.ROCK, Shape.ROCK))
+    self.assertEqual(parse_round_2('B X'), (Shape.PAPER, Shape.ROCK))
+    self.assertEqual(parse_round_2('C Z'), (Shape.SCISSORS, Shape.ROCK))
+    self.assertEqual(parse_round_2('C X'), (Shape.SCISSORS, Shape.PAPER))
 
   def test_score_outcome(self):
     self.assertEqual(score_outcome((Shape.SCISSORS, Shape.ROCK)), 6)
@@ -17,5 +23,8 @@ class Test(TestCase):
     self.assertEqual(score_round((Shape.PAPER, Shape.ROCK)), 1)
     self.assertEqual(score_round((Shape.SCISSORS, Shape.SCISSORS)), 6)
 
-  def test_total_score(self):
-    self.assertEqual(total_score(['A Y', 'B X', 'C Z']), 15)
+  def test_total_score_1(self):
+    self.assertEqual(total_score(['A Y', 'B X', 'C Z'], parse_round_1), 15)
+
+  def test_total_score_2(self):
+    self.assertEqual(total_score(['A Y', 'B X', 'C Z'], parse_round_2), 12)
