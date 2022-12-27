dddf070719dbee9846c16fb1aba5c987099c92ab
Charlie Brooking <cbrookin@thoughtworks.com>
2022-12-28 08:09:52 +1000

    simplify solution for day 6

--- 6/main.py
+++ 6/main.py
@@ -1,24 +1,19 @@
 from sys import stdin
 from functools import reduce
 
-def start_finder(n):
-  def find_start(line):
-    def reducer(window_at, char_at):
-      if len(set(window_at[1])) == n:
-        return window_at
-      window = (
-        [*window_at[1], char_at[1]] if len(window_at[1]) < n else
-        [*window_at[1][1:], char_at[1]]
-      )
-      return (char_at[0], window)
-    result = reduce(reducer, enumerate(line), (-1, []))
-    return result[0] + 1 if len(set(result[1])) == n else None
-  return find_start
+def find_start(marker_len, line):
+  def reducer(window_at, char_at):
+    return (
+      window_at if len(set(window_at[1])) == marker_len else
+      (char_at[0], [*window_at[1][-(marker_len - 1):], char_at[1]])
+    )
+  result = reduce(reducer, enumerate(line), (-1, []))
+  return result[0] + 1 if len(set(result[1])) == marker_len else None
 
 def main():
   lines = stdin.read().splitlines()
-  print(start_finder(4)(lines[0]))
-  print(start_finder(14)(lines[0]))
+  print(find_start(4, lines[0]))
+  print(find_start(14, lines[0]))
 
 if __name__ == "__main__":
     main()
--- 6/test.py
+++ 6/test.py
@@ -1,18 +1,18 @@
 from unittest import TestCase
-from main import start_finder
+from main import find_start
 
 class Test(TestCase):
-  def test_start_finder(self):
-    self.assertEqual(start_finder(4)(''), None)
-    self.assertEqual(start_finder(4)('abc'), None)
-    self.assertEqual(start_finder(4)('abcd'), 4)
-    self.assertEqual(start_finder(4)('abcde'), 4)
-    self.assertEqual(start_finder(4)('bvwbjplbgvbhsrlpgdmjqwftvncz'), 5)
-    self.assertEqual(start_finder(4)('nppdvjthqldpwncqszvftbrmjlhg'), 6)
-    self.assertEqual(start_finder(4)('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 10)
-    self.assertEqual(start_finder(4)('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 11)
-    self.assertEqual(start_finder(14)('mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 19)
-    self.assertEqual(start_finder(14)('bvwbjplbgvbhsrlpgdmjqwftvncz'), 23)
-    self.assertEqual(start_finder(14)('nppdvjthqldpwncqszvftbrmjlhg'), 23)
-    self.assertEqual(start_finder(14)('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 29)
-    self.assertEqual(start_finder(14)('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 26)
+  def test_find_start(self):
+    self.assertEqual(find_start(4, ''), None)
+    self.assertEqual(find_start(4, 'abc'), None)
+    self.assertEqual(find_start(4, 'abcd'), 4)
+    self.assertEqual(find_start(4, 'abcde'), 4)
+    self.assertEqual(find_start(4, 'bvwbjplbgvbhsrlpgdmjqwftvncz'), 5)
+    self.assertEqual(find_start(4, 'nppdvjthqldpwncqszvftbrmjlhg'), 6)
+    self.assertEqual(find_start(4, 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 10)
+    self.assertEqual(find_start(4, 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 11)
+    self.assertEqual(find_start(14, 'mjqjpqmgbljsphdztnvjfqwrcgsmlb'), 19)
+    self.assertEqual(find_start(14, 'bvwbjplbgvbhsrlpgdmjqwftvncz'), 23)
+    self.assertEqual(find_start(14, 'nppdvjthqldpwncqszvftbrmjlhg'), 23)
+    self.assertEqual(find_start(14, 'nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg'), 29)
+    self.assertEqual(find_start(14, 'zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw'), 26)
