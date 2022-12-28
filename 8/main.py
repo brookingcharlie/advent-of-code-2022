706c5534a6c71cbf82b09b1fc37895543361a36a
Charlie Brooking <cbrookin@thoughtworks.com>
2022-12-28 17:45:03 +1000

    add solution for day 8 part 2

--- 8/main.py
+++ 8/main.py
@@ -1,30 +1,58 @@
 from sys import stdin
+from dataclasses import dataclass
 from functools import reduce
-from itertools import count
+from itertools import count, product
+from operator import mul
+
+@dataclass
+class Tree:
+  id: int
+  height: int
 
 def parse(lines):
-  id_gen = count(1)
-  return [[(next(id_gen), int(c)) for c in line] for line in lines]
+  id_gen = count(0)
+  return [[Tree(next(id_gen), int(c)) for c in line] for line in lines]
 
-def find_visible(traversal):
+def find_visible_1(traversal):
   def reducer(state, tree):
     (found, highest) = state
-    (id, height) = tree
-    return ((found | {id}) if height > highest else found, max(highest, height))
+    return ((found | {tree.id}) if tree.height > highest else found, max(highest, tree.height))
   return reduce(reducer, traversal, (set(), -1))[0]
 
-def solve_puzzle(lines):
+def solve_puzzle_1(lines):
+  rows = parse(lines)
   traversals = (
-    (rows := parse(lines)) +
+    rows +
     [list(reversed(row)) for row in rows] +
     (columns := [[row[i] for row in rows] for i in range(len(rows[0]))]) +
     [list(reversed(column)) for column in columns]
   )
-  return len({id for traversal in traversals for id in find_visible(traversal)})
+  return len({id for traversal in traversals for id in find_visible_1(traversal)})
+
+def find_visible_2(starting_height, traversal):
+  stop_at = next((tree_at[0] + 1 for tree_at in enumerate(traversal) if tree_at[1].height >= starting_height), None)
+  return {tree.id for tree in traversal[:stop_at]}
+
+def solve_puzzle_2(lines):
+  rows = parse(lines)
+  def reducer(highest_score, index):
+    traversals = [
+      rows[index[0]][index[1] + 1:],
+      list(reversed(rows[index[0]][:index[1]])),
+      [row[index[1]] for row in rows[index[0] + 1:]],
+      list(reversed([row[index[1]] for row in rows[:index[0]]]))
+    ]
+    tree = rows[index[0]][index[1]]
+    distances = [len(find_visible_2(tree.height, traversal)) for traversal in traversals]
+    score = reduce(mul, distances, 1)
+    return max(highest_score, score)
+  indexes = product(range(len(rows)), range(len(rows[0])))
+  return reduce(reducer, indexes, -1)
 
 def main():
   lines = stdin.read().splitlines()
-  print(solve_puzzle(lines))
+  print(solve_puzzle_1(lines))
+  print(solve_puzzle_2(lines))
 
 if __name__ == "__main__":
   main()
--- 8/test.py
+++ 8/test.py
@@ -1,15 +1,21 @@
 from unittest import TestCase
-from main import parse, find_visible, solve_puzzle
+from main import Tree, parse, find_visible_1, find_visible_2, solve_puzzle_1, solve_puzzle_2
 
 class Test(TestCase):
   def test_parse(self):
     lines = ['24', '68']
-    expected = [[(1, 2), (2, 4)], [(3, 6), (4, 8)]]
+    expected = [[Tree(0, 2), Tree(1, 4)], [Tree(2, 6), Tree(3, 8)]]
     self.assertEqual(parse(lines), expected)
 
-  def test_find_visible(self):
-    traversal = [(1, 3), (2, 0), (3, 3), (4, 7), (5, 3)]
-    self.assertEqual(find_visible(traversal), {1, 4})
+  def test_find_visible_1(self):
+    traversal = [Tree(0, 3), Tree(1, 0), Tree(2, 3), Tree(3, 7), Tree(4, 3)]
+    self.assertEqual(find_visible_1(traversal), {0, 3})
+
+  def test_find_visible_2(self):
+    self.assertEqual(find_visible_2(5, [Tree(18, 4), Tree(19, 9)]), {18, 19})
+    self.assertEqual(find_visible_2(5, [Tree(16, 3), Tree(15, 3)]), {15, 16})
+    self.assertEqual(find_visible_2(5, [Tree(12, 3), Tree(7, 5), Tree(2, 3)]), {12, 7})
+    self.assertEqual(find_visible_2(5, [Tree(22, 3)]), {22})
 
   def test_solve_puzzle(self):
     lines = [
@@ -19,4 +25,5 @@ class Test(TestCase):
       '33549',
       '35390',
     ]
-    self.assertEqual(solve_puzzle(lines), 21)
+    self.assertEqual(solve_puzzle_1(lines), 21)
+    self.assertEqual(solve_puzzle_2(lines), 8)
