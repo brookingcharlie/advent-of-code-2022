4edabc821f53c423cfa94235e3626a497efb928c
Charlie Brooking <cbrookin@thoughtworks.com>
2023-04-29 23:24:57 +1000

    optimise solution for day 16

--- 16/main.py
+++ 16/main.py
@@ -16,27 +16,37 @@ def parse_valves(lines):
     return Valve(label, int(rate_str), leads_to)
   return [parse_valve(line) for line in lines]
 
+def infer_shortcuts(valves, start):
+  valve_map = {v.label: v for v in valves}
+  live = [v for v in valves if v.label == start or v.rate > 0]
+  def shortest(a, b, dist=0, seen=set()):
+    if b.label in a.leads_to:
+      return dist + 1
+    return min([s for l in a.leads_to if l not in seen and (s := shortest(valve_map[l], b, dist + 1, {*seen, a.label}))], default=None)
+  return {
+    a.label: {b.label: s for b in live if a != b and b.label != start and (s := shortest(a, b))}
+    for a in live
+  }
+
 def find_solution(all_valves, time, num_actors=1):
-  valve_map = {v.label: v for v in all_valves}
-  @cache
-  def find_max(time_left, open_valves, positions):
-    if time_left == 0:
-      return 0
-    combined_rate = sum(valve_map[valve].rate for valve in open_valves)
-    if len(open_valves) == len(all_valves):
-      return time_left * combined_rate
-    possible_next_steps = product(*([('open', v), *[('move', l) for l in valve_map[v].leads_to]] for v in positions))
-    options = []
-    for next_step in possible_next_steps:
-      to_open = frozenset([part[1] for part in next_step if part[0] == 'open'])
-      new_open_valves = frozenset([*open_valves, *to_open]) if not to_open.issubset(open_valves) else open_valves
-      new_positions = tuple(sorted(part[1] if part[0] == 'move' else positions[i] for (i, part) in enumerate(next_step)))
-      if new_open_valves != open_valves or new_positions != positions:
-        options.append(find_max(time_left - 1, new_open_valves, new_positions))
-    return combined_rate + max(options, default=0)
   start = 'AA'
-  open_valves = frozenset(valve.label for valve in all_valves if valve.rate == 0)
-  return find_max(time, open_valves, tuple(start for _ in range(num_actors)))
+  valve_map = {v.label: v for v in all_valves}
+  shortcuts = infer_shortcuts(all_valves, start)
+  @cache
+  def generate_options(time, total, open_valves, position):
+    current_rate = sum(valve_map[valve].rate for valve in open_valves)
+    possible_new_positions = [p for p in shortcuts[position] if p not in open_valves and time > shortcuts[position][p] + 1]
+    yield (total + time * current_rate, open_valves)
+    for new_position in possible_new_positions:
+      new_open_valves = frozenset([*open_valves, new_position])
+      new_time = time - shortcuts[position][new_position] - 1
+      new_total = total + (time - new_time) * current_rate
+      yield from generate_options(new_time, new_total, new_open_valves, new_position)
+  options = list(generate_options(time, 0, frozenset(), start))
+  if num_actors == 1:
+    return max(total for (total, _) in options)
+  elif num_actors == 2:
+    return max(t1 + t2 for ((t1, o1), (t2, o2)) in product(options, options) if o1.isdisjoint(o2))
 
 def solve_puzzle(lines):
   all_valves = parse_valves(lines)
--- 16/test.py
+++ 16/test.py
@@ -1,5 +1,5 @@
-from unittest import TestCase
-from main import Valve, parse_valves, solve_puzzle
+from unittest import TestCase, skip
+from main import Valve, parse_valves, infer_shortcuts, solve_puzzle
 
 class Test(TestCase):
   example_lines = [
@@ -19,6 +19,19 @@ class Test(TestCase):
     valves = parse_valves(Test.example_lines)
     self.assertEqual(valves[0], Valve('AA', 0, ['DD', 'II', 'BB']))
     self.assertEqual(valves[-1], Valve('JJ', 21, ['II']))
+
+  def test_infer_shortcuts(self):
+    actual = infer_shortcuts(parse_valves(Test.example_lines), 'AA')
+    expected = {
+      'AA': {'BB': 1, 'CC': 2, 'DD': 1, 'EE': 2, 'HH': 5, 'JJ': 2},
+      'BB': {'CC': 1, 'DD': 2, 'EE': 3, 'HH': 6, 'JJ': 3},
+      'CC': {'BB': 1, 'DD': 1, 'EE': 2, 'HH': 5, 'JJ': 4},
+      'DD': {'BB': 2, 'CC': 1, 'EE': 1, 'HH': 4, 'JJ': 3},
+      'EE': {'BB': 3, 'CC': 2, 'DD': 1, 'HH': 3, 'JJ': 4},
+      'HH': {'BB': 6, 'CC': 5, 'DD': 4, 'EE': 3, 'JJ': 7},
+      'JJ': {'BB': 3, 'CC': 4, 'DD': 3, 'EE': 4, 'HH': 7},
+    }
+    self.assertEqual(actual, expected)
 
   def test_solve_puzzle(self):
     self.assertEqual(solve_puzzle(Test.example_lines), (1651, 1707))
