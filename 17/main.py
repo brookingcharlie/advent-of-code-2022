9b3c4cdc878b9f102fe88350c4e1700bccfdbffd
Charlie Brooking <cbrookin@thoughtworks.com>
2023-05-08 22:57:42 +1000

    add solution for day 17 part 2

--- 17/main.py
+++ 17/main.py
@@ -4,49 +4,81 @@ from dataclasses import dataclass, field, InitVar
 from typing import ClassVar, Iterator
 from itertools import cycle, islice, repeat
 
-class Direction(Enum):
-  RIGHT = auto()
-  LEFT = auto()
-
 @dataclass
 class Cave:
-  width: ClassVar[int] = 7
-  start_x: ClassVar[int] = 2
-  start_y_offset: ClassVar[int] = 3
-  jets: InitVar[list[Direction]]
-  jet_generator: Iterator[Direction] = None
-  rocks: set[tuple[int, int]] = field(default_factory=set)
+  __width: ClassVar[int] = 7
+  __start_x: ClassVar[int] = 2
+  __start_y_offset: ClassVar[int] = 3
+  __band_size: ClassVar[int] = 50
+
+  jets: InitVar[list[int]]
   height: int = 0
 
-  def __post_init__(self, jets):
-    self.jet_generator = cycle(jets)
+  __jet_generator: Iterator[int] = None
+  __rocks: set[tuple[int, int]] = field(default_factory=set)
+  __patterns_seen: dict[tuple[frozenset[tuple[int, int]], int, int], tuple[int, int]] = field(default_factory=dict)
+  __repeated_band: tuple[int, int] = None
 
-  def add_rock(self, rock):
-    pre_height = self.height
-    start_y = pre_height + self.start_y_offset
-    position = self.start_x, start_y
-    min_x, max_x = 0, Cave.width - max(x for x, _ in rock) - 1
-    for y in reversed(range(start_y + 1)):
-      shift_x = -1 if next(self.jet_generator) == Direction.LEFT else 1
-      x_move = (max(min_x, min(max_x, position[0] + shift_x)), y)
-      if set((x_move[0] + x, x_move[1] + y) for (x, y) in rock).isdisjoint(self.rocks):
+  def __post_init__(self, jets):
+    self.__jet_generator = cycle(enumerate(jets))
+
+  def add_rocks(self, rock_shapes, num_rocks):
+    rock_generator = enumerate(islice(cycle(rock_shapes), num_rocks))
+    num_repeated_bands, repeated_band_rocks, repeated_band_height = None, None, None
+    for rock in rock_generator:
+      if num_repeated_bands and rock[0] + num_repeated_bands * repeated_band_rocks == num_rocks:
+        self.height += num_repeated_bands * repeated_band_height
+        break
+      self.__add_rock(rock, len(rock_shapes))
+      if num_repeated_bands is None and self.__repeated_band:
+        repeated_band_rocks, repeated_band_height = self.__repeated_band
+        num_repeated_bands = (num_rocks - rock[0]) // repeated_band_rocks
+
+  def __add_rock(self, rock, num_rock_shapes):
+    position = self.__find_position(rock, num_rock_shapes)
+    self.__commit_rock(position, rock)
+
+  def __find_position(self, rock, num_rock_shapes):
+    min_x, max_x = 0, Cave.__width - max(x for (x, _) in rock[1]) - 1
+    start_y = self.height + self.__start_y_offset
+    position = (self.__start_x, start_y)
+    for (y, jet) in zip(reversed(range(start_y + 1)), self.__jet_generator):
+      if y == start_y:
+        self.__check_for_repeat(y, rock, num_rock_shapes, jet)
+      x_move = (max(min_x, min(max_x, position[0] + jet[1])), y)
+      if self.__unobstructed(x_move, rock):
         position = x_move
-      if y > 0:
-        y_move = (position[0], y - 1)
-        if set((y_move[0] + x, y_move[1] + y) for (x, y) in rock).isdisjoint(self.rocks):
-          position = y_move
-        else:
-          break
-    for (x, y) in rock:
+      y_move = (position[0], y - 1)
+      if y_move[1] >= 0 and self.__unobstructed(y_move, rock):
+        position = y_move
+      else:
+        break
+    return position
+
+  def __check_for_repeat(self, y, rock, num_rock_shapes, jet):
+    if not self.__repeated_band:
+      band = frozenset((x, self.height - y) for (x, y) in self.__rocks if self.height - y < self.__band_size),
+      scenario = (band, rock[0] % num_rock_shapes, jet[0])
+      if scenario in self.__patterns_seen:
+        (rocks_seen, height_seen) = self.__patterns_seen[scenario]
+        self.__repeated_band = (rock[0] - rocks_seen, self.height - height_seen)
+      else:
+        self.__patterns_seen[scenario] = (rock[0], self.height)
+
+  def __unobstructed(self, position, rock):
+    return set((position[0] + x, position[1] + y) for (x, y) in rock[1]).isdisjoint(self.__rocks)
+
+  def __commit_rock(self, position, rock):
+    for (x, y) in rock[1]:
       rock_point = (position[0] + x, position[1] + y)
-      self.rocks.add(rock_point)
+      self.__rocks.add(rock_point)
       self.height = max(self.height, rock_point[1] + 1)
 
   def draw(self):
     def line(y):
-      return ('#' if (x, y) in self.rocks else '.' for x in range(Cave.width))
+      return ('#' if (x, y) in self.__rocks else '.' for x in range(Cave.__width))
     lines = (f"|{''.join(line(y))}|" for y in reversed(range(self.height)))
-    base = f"+{''.join(repeat('-', Cave.width))}+"
+    base = f"+{''.join(repeat('-', Cave.__width))}+"
     return '\n'.join([*lines, base])
 
 rock_shapes = [
@@ -57,23 +89,20 @@ rock_shapes = [
   [(0, 0), (1, 0), (0, 1), (1, 1)]
 ]
 
-def rock_generator(num_rocks):
-  return islice(cycle(rock_shapes), num_rocks)
-
 def parse_jets(line):
-  lookup = {'<': Direction.LEFT, '>': Direction.RIGHT}
+  lookup = {'<': -1, '>': 1}
   return [lookup[char] for char in line]
 
 def solve_puzzle(line, num_rocks):
   jets = parse_jets(line)
   cave = Cave(jets)
-  for rock in rock_generator(num_rocks):
-    cave.add_rock(rock)
+  cave.add_rocks(rock_shapes, num_rocks)
   return cave.height
 
 def main():
   line = sys.stdin.read().splitlines()[0]
   print(solve_puzzle(line, 2022))
+  print(solve_puzzle(line, 1000000000000))
 
 if __name__ == "__main__":
   main()
--- 17/test.py
+++ 17/test.py
@@ -1,18 +1,15 @@
 from unittest import TestCase
-from main import Direction, Cave, rock_generator, parse_jets, solve_puzzle
+from main import Cave, parse_jets, rock_shapes, solve_puzzle
 
 class Test(TestCase):
   line = '>>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>'
 
   def test_parse_jets(self):
-    actual = parse_jets('><<>')
-    expected = [Direction.RIGHT, Direction.LEFT, Direction.LEFT, Direction.RIGHT]
-    self.assertEqual(actual, expected)
+    self.assertEqual(parse_jets('><<>'), [1, -1, -1, 1])
 
   def test_cave(self):
     cave = Cave(parse_jets(Test.line))
-    for rock in rock_generator(10):
-      cave.add_rock(rock)
+    cave.add_rocks(rock_shapes, 10)
     drawing = (
       '|....#..|\n'
       '|....#..|\n'
@@ -38,3 +35,4 @@ class Test(TestCase):
 
   def test_solve_puzzle(self):
     self.assertEqual(solve_puzzle(Test.line, 2022), 3068)
+    self.assertEqual(solve_puzzle(Test.line, 1000000000000), 1514285714288)
