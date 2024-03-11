48ddd83447874a9d411c4b6b0c4a348cd267ee54
Charlie Brooking <cbrookin@thoughtworks.com>
2024-03-11 18:29:53 +1100

    add solution for day 18 part 2

--- 18/main.py
+++ 18/main.py
@@ -2,19 +2,47 @@ import sys
 import re
 
 def parse_cubes(lines):
-  return [tuple(int(n) for n in re.findall(r'\d+', line)) for line in lines]
+  return set(tuple(int(n) for n in re.findall(r'\d+', line)) for line in lines)
+
+def adjacent_points(point):
+  deltas = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
+  return set(tuple(map(sum, zip(point, delta))) for delta in deltas)
+
+def create_bounding_box(cubes):
+  return tuple((min(d) - 1, max(d) + 1) for d in [[c[i] for c in cubes] for i in range(3)])
+
+def outside_bounding_box(bounding_box, point):
+  return any(p < b[0] or p > b[1] for (p, b) in zip(point, bounding_box))
+
+def solve_puzzle_1(cubes):
+  return sum(len(adjacent_points(cube) - cubes) for cube in cubes)
+
+def solve_puzzle_2(cubes):
+  bounding_box = create_bounding_box(cubes)
+  start_point = tuple(b[0] for b in bounding_box)
+  remaining_points = set([start_point])
+  visited_points = set()
+  surface_area = 0
+  while len(remaining_points) > 0:
+    point = remaining_points.pop()
+    reachable_points = set(
+      p for p in adjacent_points(point)
+      if not outside_bounding_box(bounding_box, p) and p not in visited_points
+    )
+    reachable_cubes = reachable_points.intersection(cubes)
+    remaining_points.update(reachable_points.difference(reachable_cubes))
+    visited_points.add(point)
+    surface_area += len(reachable_cubes)
+  return surface_area
 
 def solve_puzzle(lines):
-  cubes = set(parse_cubes(lines))
-  def sides_exposed(cube):
-    deltas = [(-1, 0, 0), (1, 0, 0), (0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1)]
-    adjacent_points = [tuple(map(sum, zip(cube, delta))) for delta in deltas]
-    return sum(1 for p in adjacent_points if p not in cubes)
-  return sum(sides_exposed(cube) for cube in cubes)
+  cubes = parse_cubes(lines)
+  return (solve_puzzle_1(cubes), solve_puzzle_2(cubes))
 
 def main():
   lines = sys.stdin.read().splitlines()
-  print(solve_puzzle(lines))
+  for solution in solve_puzzle(lines):
+    print(solution)
 
 if __name__ == "__main__":
   main()
--- 18/test.py
+++ 18/test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase
-from main import parse_cubes, solve_puzzle
+from main import parse_cubes, create_bounding_box, outside_bounding_box, solve_puzzle
 
 class Test(TestCase):
   lines = [
@@ -20,8 +20,20 @@ class Test(TestCase):
 
   def test_parse_cubes(self):
     cubes = parse_cubes(Test.lines)
-    self.assertEqual(cubes[0], (2, 2, 2))
-    self.assertEqual(cubes[-1], (2, 3, 5))
+    self.assertIn((2, 2, 2), cubes)
+    self.assertIn((2, 3, 5), cubes)
+
+  def test_create_bounding_box(self):
+    actual = create_bounding_box(parse_cubes(Test.lines))
+    expected = ((0, 4), (0, 4), (0, 7))
+    self.assertEqual(actual, expected)
+
+  def test_outside_bounding_box(self):
+    bounding_box = ((0, 2), (0, 4), (0, 3))
+    self.assertEqual(outside_bounding_box(bounding_box, (0, 3, 2)), False)
+    self.assertEqual(outside_bounding_box(bounding_box, (2, 0, 0)), False)
+    self.assertEqual(outside_bounding_box(bounding_box, (3, 1, 2)), True)
+    self.assertEqual(outside_bounding_box(bounding_box, (0, 5, 4)), True)
 
   def test_solve_puzzle(self):
-    self.assertEqual(solve_puzzle(Test.lines), 64)
+    self.assertEqual(solve_puzzle(Test.lines), (64, 58))
